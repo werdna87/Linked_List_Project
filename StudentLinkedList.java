1369dd9084ec8a397545f849e6148c4cab6afb58
Andrew Borghesani <coolkid999@gmail.com>
2013-03-27 11:42:26 -0400

    Initial

---
+++ StudentLinkedList.java
@@ -0,0 +1,20 @@
+
+public class StudentLinkedList {
+	StudentNode head = new StudentNode(new Student("","",0),null);
+	public String toString(){
+		return "";
+	}
+	public void insetByLastName(String fName, String lName, int GPA){
+		Student s = new Student(fName,lName,GPA);
+	}
+	public Student removeStudent(String fName, String lName){
+		return null;
+	}
+	public void sortByAverage(){
+		
+	}
+	public static void main(String[] args){
+		
+	}
+
+}
